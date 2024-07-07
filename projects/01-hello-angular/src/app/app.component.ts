f9a8e1c537feb36d3a2b60f45419109f87a41b79
Ibrahim <ibrahim.borba@gmail.com>
2024-07-07 16:16:37 -0300

    feat: add css and html elements

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,16 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+    <ol>
+      <li>Song</li>
+      <li>Movie</li>
+      <li>Book</li>
+    </ol>
   `,
-  styles: ``,
+  styles: `
+    ol {
+      list-style-type: upper-roman;
+    }
+  `,
 })
 export class AppComponent {}
