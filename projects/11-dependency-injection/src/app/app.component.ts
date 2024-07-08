0ebf5e1cfddf0e7ccb54cef2babd8bade0651878
Ibrahim <ibrahim.borba@gmail.com>
2024-07-08 06:47:12 -0300

    feat: add dependency injection

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: `
+    <h1>User Listing</h1>
+    @for (userEntry of userData; track userEntry) {
+      <app-user-info [user]="userEntry"/>
+    }
+  `,
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  userService = inject(UserService);
+  userData: User[] = [];
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.userService.getUserData();
+    this.userData = data
+  }
+  constructor() {
+    this.userService.getUserData().then((data) => { this.userData = data});
+  }
 }
